4c38459675d660224c557fd1eb3568777a286d0a
Ivan Griessel <ivangriessel@gmail.com>
2024-07-25 15:57:02 +0100

    Revert "Replace basic website with new rails app"
    This reverts commit 68080853f72213dd04402c8618ad4d8aa33df039.

--- app/controllers/pages_controller.rb
+++
@@ -1,8 +0,0 @@
-class PagesController < ApplicationController
-  def home
-    #by convention this method will expect a home.html.erb file in the views/pages folder
-  end
-
-  def about
-  end
-end
--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  root 'pages#home' #send the application to the hello method in the application controller
-  get 'about', to: 'pages#about'
-end
